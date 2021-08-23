e3354c2461c03de07cf40eae5559eb9362355f35
jonnii <me@jonnii.com>
2021-08-23 12:41:55 -0400

    try adding a filter

--- src/main/kotlin/com/http2error/TestController.kt
+++ src/main/kotlin/com/http2error/TestController.kt
@@ -1,17 +1,38 @@
 package com.http2error
 
+import io.micrometer.core.instrument.MeterRegistry
+import io.micronaut.context.annotation.Requirements
+import io.micronaut.context.annotation.Requires
+import io.micronaut.core.async.publisher.Publishers
+import io.micronaut.http.HttpAttributes
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Filter
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Header
 import io.micronaut.http.annotation.QueryValue
 import io.micronaut.http.context.ServerRequestContext
+import io.micronaut.http.filter.HttpServerFilter
+import io.micronaut.http.filter.ServerFilterChain
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
+import io.micronaut.web.router.RouteMatch
+import io.micronaut.web.router.UriRouteMatch
+import org.reactivestreams.Publisher
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.Optional
 import java.util.function.Function
+
+@Filter("/**")
+class SomeFilter : HttpServerFilter {
+    override fun doFilter(request: HttpRequest<*>, chain: ServerFilterChain): Publisher<MutableHttpResponse<*>> {
+        return Publishers.map(chain.proceed(request)) {
+            it
+        }
+    }
+}
 
 @Controller
 open class TestController {
